c46ead2329ff9e98540518d78e12f026782932f6
Snehal Tikhe <snehaltikhe@Snehals-MacBook-Pro.local>
2019-02-07 01:23:12 -0500

    Login Page

---
+++ Chat Room/View/GradientView.swift
@@ -0,0 +1,21 @@
+//
+//  GradientView.swift
+//  Chat Room
+//
+//  Created by Snehal Tikhe on 2/6/19.
+//  Copyright © 2019 Snehal Tikhe. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
